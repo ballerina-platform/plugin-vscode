513e2738e503b347f867d24c240a734da60594df
Prabushi Samarakoon <prabushisamarakoon@gmail.com>
2021-03-17 23:10:42 +0530

    Integrate lowcode editor package

--- src/core/extended-language-client.ts
+++ src/core/extended-language-client.ts
@@ -18,8 +18,14 @@
  *
  */
 
-import { LanguageClient, TextDocumentPositionParams } from "vscode-languageclient";
+import { DocumentSymbol, DocumentSymbolParams, LanguageClient, SymbolInformation, TextDocumentPositionParams } from "vscode-languageclient";
 import { Uri, Location } from "vscode";
+import { LowCodeLangClient } from "@drifftr/ballerina-low-code-editor";
+import {
+    DidOpenParams, DidCloseParams, DidChangeParams, BallerinaSyntaxTreeModifyRequest, BallerinaSyntaxTreeResponse, BallerinaConnectorsResponse,
+    BallerinaConnectorRequest, BallerinaConnectorResponse, BallerinaRecordRequest, BallerinaRecordResponse, BallerinaSTModifyRequest, BallerinaSTModifyResponse,
+    TriggerModifyRequest, GetSyntaxTreeParams, GetSyntaxTreeResponse, CompletionParams, CompletionResponse,
+} from "@drifftr/ballerina-low-code-editor/build/Definitions";
 
 export const BALLERINA_LANG_ID = "ballerina";
 
@@ -28,9 +34,9 @@ export interface BallerinaSyntaxTree {
     topLevelNodes: any[];
 }
 
-export interface BallerinaSyntaxTreeResponse {
-    syntaxTree?: BallerinaSyntaxTree;
-}
+// export interface BallerinaSyntaxTreeResponse {
+//     syntaxTree?: BallerinaSyntaxTree;
+// }
 
 export interface GetSyntaxTreeRequest {
     documentIdentifier: {
@@ -111,7 +117,80 @@ export interface GetSynRequest {
     Params: string;
 }
 
-export class ExtendedLangClient extends LanguageClient {
+export interface LowCodeLangClient1 extends Omit<LowCodeLangClient, 'diagnostics'> {
+    // Diagnostics: (params: BallerinaProjectParams) => Thenable<PublishDiagnosticsParams[]>;
+}
+
+export class ExtendedLangClient extends LanguageClient implements LowCodeLangClient1 {
+    // Diagnostics(params: BallerinaProjectParams): Thenable<PublishDiagnosticsParams[]> {
+    //     return this.sendRequest<PublishDiagnosticsParams[]>("ballerinaDocument/diagnostics", params);
+    // }
+    isInitialized: boolean = true;
+
+    didOpen(params: DidOpenParams): void {
+        this.sendRequest("textDocument/didOpen", params);
+    }
+    registerPublishDiagnostics(): void {
+        this.onNotification("textDocument/publishDiagnostics", (notification: any) => {
+            // const { diagnostics, uri } = notification;
+            // if (uri && uri === ("file://" + store.getState().appInfo?.currentApp?.workingFile)) {
+            // store.dispatch(diagramSetDiagnostic(diagnostics));
+            // }
+        });
+    }
+    didClose(params: DidCloseParams): void {
+        this.sendRequest("textDocument/didClose", params);
+    }
+    didChange(params: DidChangeParams): void {
+        this.sendRequest("textDocument/didChange", params);
+    }
+    syntaxTreeModify(params: BallerinaSyntaxTreeModifyRequest): Thenable<BallerinaSyntaxTreeResponse> {
+        return this.sendRequest<BallerinaSyntaxTreeResponse>("ballerinaDocument/syntaxTreeModify", params);
+    }
+    getConnectors(): Thenable<BallerinaConnectorsResponse> {
+        return this.sendRequest<BallerinaConnectorsResponse>("ballerinaConnector/connectors");
+    }
+    getConnector(params: BallerinaConnectorRequest): Thenable<BallerinaConnectorResponse> {
+        return this.sendRequest<BallerinaConnectorResponse>("ballerinaConnector/connector", params);
+    }
+    getRecord(params: BallerinaRecordRequest): Thenable<BallerinaRecordResponse> {
+        return this.sendRequest<BallerinaRecordResponse>("ballerinaConnector/record", params);
+    }
+    astModify(params: BallerinaSTModifyRequest): Thenable<BallerinaSTModifyResponse> {
+        return this.sendRequest<BallerinaSTModifyResponse>("ballerinaDocument/astModify", params);
+    }
+    stModify(params: BallerinaSTModifyRequest): Thenable<BallerinaSTModifyResponse> {
+        return this.sendRequest<BallerinaSTModifyResponse>("ballerinaDocument/syntaxTreeModify", params);
+    }
+    triggerModify(params: TriggerModifyRequest): Thenable<BallerinaSTModifyResponse> {
+        return this.sendRequest<BallerinaSTModifyResponse>("ballerinaDocument/triggerModify", params);
+    }
+
+    getCompletion(params: CompletionParams): Thenable<CompletionResponse[]> {
+        return this.sendRequest("textDocument/completion", params);
+    }
+
+    public getDocumentSymbol(params: DocumentSymbolParams): Thenable<DocumentSymbol[] | SymbolInformation[] | null> {
+        return this.sendRequest("textDocument/documentSymbol", params);
+    }
+
+    public close(): void {
+        // this.shutdown();
+    }
+    getDidOpenParams(): DidOpenParams {
+        // const uri = store.getState().appInfo?.currentApp?.workingFile;
+        // const content = store.getState()?.appInfo?.currentFile?.content;
+        // const decContent = content ? atob(content) : "";
+        return {
+            textDocument: {
+                uri: "file://",
+                languageId: "ballerina",
+                text: '',
+                version: 1
+            }
+        };
+    }
+
     getSyntaxHighlighter(params: string): Thenable<BallerinaSynResponse> {
         const req: GetSynRequest = {
             Params: params
@@ -119,7 +198,7 @@ export class ExtendedLangClient extends LanguageClient {
         return this.sendRequest("ballerinaSyntaxHighlighter/list", req);
     }
 
-    getSyntaxTree(uri: Uri): Thenable<BallerinaSyntaxTreeResponse> {
+    getSyntaxTree(uri: GetSyntaxTreeParams): Thenable<GetSyntaxTreeResponse> {
         const req: GetSyntaxTreeRequest = {
             documentIdentifier: {
                 uri: uri.toString()
